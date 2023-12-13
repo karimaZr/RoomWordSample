445e6adb31af996b70b4c5d513983a169fa0278c
karimaZr <128175856+karimaZr@users.noreply.github.com>
2023-12-13 22:42:25 +0100

    Initial commit

---
+++ app/src/main/java/ensaj/ma/roomwordsample/WordViewModel.java
@@ -0,0 +1,28 @@
+package ensaj.ma.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import ensaj.ma.roomwordsample.data.WordRepository;
+import ensaj.ma.roomwordsample.model.Word;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
---
+++ app/src/main/java/ensaj/ma/roomwordsample/model/Word.java
@@ -0,0 +1,40 @@
+package ensaj.ma.roomwordsample.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String word;
+
+    public Word() {
+    }
+
+    public Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(@NonNull String word) {
+        this.word = word;
+    }
+}
